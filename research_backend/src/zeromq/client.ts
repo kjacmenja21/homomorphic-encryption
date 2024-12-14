5075253adb863dc42a6c622005912e881808c9b3
kjacmenja21 <karlojacmenjak@gmail.com>
2024-12-14 17:48:41 +0100

    Create primitive server

---
+++ research_backend/src/zeromq/client.ts
@@ -0,0 +1,23 @@
+import { Request } from "zeromq";
+
+const ZEROMQ_PORT = process.env.ZEROMQ_PORT;
+const ZEROMQ_HOST = process.env.ZEROMQ_HOST;
+
+export async function runClient(message: string) {
+  const client = new Request();
+
+  try {
+    await client.connect(`tcp://${ZEROMQ_HOST}:${ZEROMQ_PORT}`);
+    console.log("Client connected to server.");
+
+    console.log(`Sending request: ${message}`);
+    await client.send(message);
+
+    const [reply] = await client.receive();
+    console.log(`Received reply: ${reply.toString()}`);
+  } catch (error) {
+    console.error("Error:", error);
+  } finally {
+    await client.close();
+  }
+}
