d0cce1d20d42846fbb89b0d8d6e9b766fbeb465d
kjacmenja21 <karlojacmenjak@gmail.com>
2024-12-15 14:26:16 +0100

    Init WebSocketServer

--- research_backend/src/main.ts
+++ research_backend/src/main.ts
@@ -1,14 +1,19 @@
-const express = require("express");
 import "dotenv/config";
+import express from "express";
+import http from "http";
 import { apiRouter } from "./api/api";
-
-const app = express();
+import { setupWebSocketServer } from "./ws/ws";
 
 const HOST = process.env.HOST;
 const PORT = process.env.PORT;
 
+const app = express();
+const server = http.createServer(app); // Create the HTTP server for Express
+
 app.use(express.json());
 app.use("/", apiRouter);
+
+setupWebSocketServer(server);
 
 app.listen(PORT, () => {
   console.log(`Server running on ${HOST}:${PORT}`);
