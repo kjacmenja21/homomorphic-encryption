790046b00ab159cd926517e0482768c7966a9e9c
kjacmenja21 <karlojacmenjak@gmail.com>
2024-12-16 14:37:31 +0100

    Fix not auto updating interval

--- research_backend/src/zeromq/client.ts
+++ research_backend/src/zeromq/client.ts
@@ -13,23 +13,20 @@ class ZeroMQClient {
   url: string;
   config: Config;
   client: Request;
+  private isRunning: boolean;
+
   constructor(host: string, port: string, config: Config) {
     this.url = `tcp://${host}:${port}`;
     this.config = config;
     this.client = new Request();
+    this.isRunning = true;
   }
 
   start = async () => {
     try {
       this.connect().then(() => {
         console.log("Client connected to server.");
-
-        setInterval(async () => {
-          const result = await this.send(
-            JSON.stringify({ type: this.config.request_type })
-          );
-          this.handleRecieves(result);
-        }, this.config.request_interval_seconds * 1000);
+        this.scheduleRequests();
       });
     } catch (err) {
       console.error("Error connecting to ZeroMQ:", err);
@@ -54,6 +51,22 @@ class ZeroMQClient {
 
     const [reply] = await this.client.receive();
     return reply;
+  };
+
+  private scheduleRequests = async () => {
+    if (!this.isRunning) return;
+
+    try {
+      const result = await this.send(
+        JSON.stringify({ type: this.config.request_type })
+      );
+      this.handleRecieves(result);
+    } catch (err) {
+      console.error("Error sending request:", err);
+    }
+
+    const intervalMs = this.config.request_interval_seconds * 1000;
+    setTimeout(this.scheduleRequests, intervalMs);
   };
 
   handleRecieves = (result: Message) => {
